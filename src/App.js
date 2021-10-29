7b79255457d8dd4494bf90c5617e7fc6234722f0
Pinny Geliebter <pgibralter@gmail.com>
2021-10-29 12:59:14 -0400

    finishes exercise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import './style.css'
 import TodoItem from './TodoItem'
+import todosData from './todosData'
 
+const cee = todosData.map((ele) => {
+	return <TodoItem todoItem={ele} key={ele.id} />
+})
 function App() {
-	return (
-		<div className="todo-list">
-			<TodoItem />
-			<TodoItem />
-			<TodoItem />
-		</div>
-	)
+	return <div className="todo-list">{cee}</div>
 }
 export default App
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function TodoItem() {
+function TodoItem(props) {
 	return (
 		<div className="todo-item">
-			<input type="checkbox" />
-			<p>Placeholder text here</p>
+			<input type="checkbox" defaultChecked={props.todoItem.completed} />
+			<p> {props.todoItem.text}</p>
 		</div>
 	)
 }
