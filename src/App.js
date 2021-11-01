f6aeba4a5b3566cee3231141e51d9bddda3854f5
Pinny Geliebter <pgibralter@gmail.com>
2021-11-01 08:17:03 -0400

    adds a button to change isLoggedIn state and rerender page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './style.css'
-function App() {
-	return <div>Hi!</div>
+
+// Given a stateless functional component, add state to it
+// state should have a property called `isLoggedIn` which is a boolean
+// (true if logged in, false if not)
+// Then, give your best shot at rendering the word "in" if the user is logged in
+// or "out" if the user is logged out.
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = { isLoggedIn: false }
+	}
+	render() {
+		return (
+			<div>
+				<h1>
+					You are currently logged{' '}
+					{this.state.isLoggedIn ? ' in' : ' out'}
+				</h1>
+				<button
+					onClick={() => {
+						console.log(this.state.isLoggedIn)
+						this.setState({ isLoggedIn: !this.state.isLoggedIn })
+					}}
+				>
+					{this.state.isLoggedIn ? 'logged in' : 'logged out'}
+				</button>
+			</div>
+		)
+	}
 }
+
 export default App
