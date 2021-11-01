e97abc59ab6e96aededf0cbd38896c194d642d3f
Pinny Geliebter <pgibralter@gmail.com>
2021-11-01 08:31:21 -0400

    updates todos to use state to initalize our todoList

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import './style.css'
 import TodoItem from './TodoItem'
 import todosData from './todosData'
 
-const cee = todosData.map((ele) => {
-	return <TodoItem todoItem={ele} key={ele.id} />
-})
-function App() {
-	return <div className="todo-list">{cee}</div>
+class App extends React.Component {
+	constructor() {
+		super()
+		this.state = { todosDataState: todosData }
+	}
+	render() {
+		const cee = this.state.todosDataState.map((ele) => {
+			return <TodoItem todoItem={ele} key={ele.id} />
+		})
+		return <div className="todo-list">{cee}</div>
+	}
 }
 export default App
