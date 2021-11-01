fd1f1e1595e534818f52974f3453aaebf3345d89
Pinny Geliebter <pgibralter@gmail.com>
2021-11-01 08:41:40 -0400

    adds onMouseHover over image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import './style.css'
-function App() {
-	return <div>Hi!</div>
+
+function handleClick() {
+	console.log('I was clicked')
 }
+
+function App() {
+	return (
+		<div>
+			<img
+				onMouseOver={() => console.log('hovering!')}
+				src="https://www.fillmurray.com/200/100"
+			/>
+			<br />
+			<br />
+			<button onClick={handleClick}>Click me</button>
+		</div>
+	)
+}
+
 export default App
