6a0b968f1411acea3f6b1d6bb0d9bf89a36ad6fe
Pinny Geliebter <pgibralter@gmail.com>
2021-11-01 09:06:22 -0400

    adds function to update state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React from 'react'
 import './style.css'
-function App() {
-	return <div>Hi!</div>
+
+class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			count: 0
+		}
+		this.handleClick = this.handleClick.bind(this)
+	}
+
+	handleClick() {
+		this.setState((prevState) => {
+			return { count: prevState.count + 1 }
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>{this.state.count}</h1>
+				<button onClick={this.handleClick}>Change!</button>
+			</div>
+		)
+	}
 }
+
 export default App
