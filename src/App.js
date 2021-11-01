4481f1e449b490f80f43d807ef3374baee8fd029
Pinny Geliebter <pgibralter@gmail.com>
2021-11-01 09:23:54 -0400

    creates a basic calculator

--- src/App.js
+++ src/App.js
@@ -5,22 +5,50 @@ class App extends React.Component {
 	constructor() {
 		super()
 		this.state = {
-			count: 0
+			count: 0,
+			inputValue: 0
 		}
-		this.handleClick = this.handleClick.bind(this)
+		this.handleClickAdd = this.handleClickAdd.bind(this)
+		this.handleClickDivide = this.handleClickDivide.bind(this)
+		this.handleClickMultiply = this.handleClickMultiply.bind(this)
 	}
-
-	handleClick() {
+	handleClickAdd() {
 		this.setState((prevState) => {
-			return { count: prevState.count + 1 }
+			return { count: prevState.count + this.state.inputValue }
+		})
+	}
+	updateInputValue(evt) {
+		this.setState({
+			inputValue: evt.target.value
+		})
+	}
+	handleClickDivide() {
+		this.setState((prevState) => {
+			return {
+				count: prevState.count / this.state.inputValue
+			}
+		})
+	}
+	handleClickMultiply() {
+		this.setState((prevState) => {
+			return { count: prevState.count * this.state.inputValue }
 		})
 	}
 
 	render() {
 		return (
 			<div>
-				<h1>{this.state.count}</h1>
-				<button onClick={this.handleClick}>Change!</button>
+				<h1>
+					{this.state.inputValue} &gt; {this.state.count}
+				</h1>
+				<h1 />
+				<input
+					value={this.state.inputValue}
+					onChange={(evt) => this.updateInputValue(evt)}
+				/>
+				<button onClick={this.handleClickAdd}>Add!</button>
+				<button onClick={this.handleClickDivide}>Divide </button>
+				<button onClick={this.handleClickMultiply}>multiply!</button>
 			</div>
 		)
 	}
