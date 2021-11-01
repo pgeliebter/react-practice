02447d3ef68e2e29bb18f0e1ca12624bb09563de
Pinny Geliebter <pgibralter@gmail.com>
2021-11-01 08:50:46 -0400

    adds onChange func

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,11 @@ import React from 'react'
 function TodoItem(props) {
 	return (
 		<div className="todo-item">
-			<input type="checkbox" defaultChecked={props.todoItem.completed} />
+			<input
+				type="checkbox"
+				checked={props.todoItem.completed}
+				onChange={() => console.log('howdy there')}
+			/>
 			<p> {props.todoItem.text}</p>
 		</div>
 	)
