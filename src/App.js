199f44d616182c2c7b838b57e628187b2b995aa9
Pinny Geliebter <pgibralter@gmail.com>
2021-11-03 12:43:23 -0400

    sets up beginning of lesson

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
 import React from 'react'
 import './style.css'
-function App() {
-	return <div>Hi!</div>
+import randomcolor from 'randomcolor'
+
+class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			count: 0,
+			color: ''
+		}
+		this.increment = this.increment.bind(this)
+	}
+
+	increment() {
+		this.setState((prevState) => {
+			return {
+				count: prevState.count + 1
+			}
+		})
+	}
+
+	componentDidUpdate() {
+		const newColor = randomcolor()
+		console.log(newColor)
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>{this.state.count}</h1>
+				<button onClick={this.increment}>Increment!</button>
+			</div>
+		)
+	}
 }
+
 export default App
