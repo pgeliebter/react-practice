73e6eaaae17daebc79a962394948b812790d4e09
Pinny Geliebter <pgibralter@gmail.com>
2021-11-03 15:10:48 -0400

    adds conditional render example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
-import React from 'react'
-import './style.css'
-function App() {
-	return <div>Hi!</div>
+import React, { Component } from 'react'
+import Conditional from './Conditional'
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			finishedLoading: false,
+			data: ''
+		}
+	}
+
+	componentDidMount() {
+		setTimeout(() => {
+			this.setState({
+				finishedLoading: true,
+				data: 'This is data returned from fake API'
+			})
+		}, 1500),
+			setTimeout(() => {
+				this.setState({
+					data: 'This is different data returned from fake API'
+				})
+			}, 3000)
+	}
+
+	render() {
+		return (
+			<div>
+				{this.state.finishedLoading && (
+					<Conditional
+						finishedLoading={this.state.finishedLoading}
+						data={this.state.data}
+					/>
+				)}
+			</div>
+		)
+	}
 }
+
 export default App
