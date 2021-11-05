6f8408fa524f1aa10bc76712a5d1abbae25d26c4
Pinny Geliebter <pgibralter@gmail.com>
2021-11-05 14:57:40 -0400

    adds basic fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import './style.css'
-function App() {
-	return <div>Hi!</div>
+class App extends React.Component {
+	constructor() {
+		super()
+		this.state = { character: {} }
+	}
+	componentDidMount() {
+		fetch('https://swapi.dev/api/people/1/')
+			.then((response) => response.json())
+			.then((data) => {
+				this.setState(() => {
+					return { data: data }
+				})
+			})
+	}
+
+	render() {
+		return <div>Waiting...</div>
+	}
 }
 export default App
